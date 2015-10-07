16c3091935278d4f918ac9011395f6036986938d
adamtobias <atgrimm@gmail.com>
2015-10-07 16:00:03 -0700

    Completed Linked Lists.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(!(list.tail === null)){
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var tmpVal = list.head.value;
+    list.head = list.head.next;
+    return tmpVal;
   };
 
   list.contains = function(target) {
+    var i;
+    if(list.head === null){
+      return false;
+    }
+    for(i = list.head; !(i.value === target || i.next === null); i = i.next){}
+    if(i.value === target){
+      return true;
+    }
+    return false;
   };
 
   return list;
