c010f0aafeace9985c0d8ec489f0c50173ac70bd
adamtobias <atgrimm@gmail.com>
2015-10-07 14:27:05 -0700

    Completed construction of Stack for functional class.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var length = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    var temp = storage[length-1];
+    if(length > 0){
+      length--;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
