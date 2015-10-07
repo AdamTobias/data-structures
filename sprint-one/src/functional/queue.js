4c0de983155118f81520404189c9d981f40ee48c
adamtobias <atgrimm@gmail.com>
2015-10-07 14:36:54 -0700

    Finshed queue for functional class.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var start = 0;
+  var end = 0;  
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function() {
+    var tmp;
+    if (end - start > 0) {
+      tmp = storage[start];
+      start++;
+    } 
+    return tmp;
   };
 
   someInstance.size = function() {
+    return end - start;
   };
 
   return someInstance;
