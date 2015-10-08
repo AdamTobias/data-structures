f82953fb2ec8a30c2236dfd6cb91f0d693dd0289
adamtobias <atgrimm@gmail.com>
2015-10-08 15:59:46 -0700

    Completed hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var hashObj = this._storage.get(index) || {};
+  hashObj[k] = v;
+  this._storage.set(index, hashObj);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tmp = this._storage.get(index);
+  if (tmp === undefined || _.isEqual(tmp, {})) {
+    return null;
+  } else {
+    return this._storage.get(index)[k];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var hashObj = this._storage.get(index) || {};
+
+  delete hashObj[k];
+
+  this._storage.set(index, hashObj);
 };
 
 
