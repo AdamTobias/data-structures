dfbf427ae56da8d1387f9835eac124d2bd09b872
adamtobias <atgrimm@gmail.com>
2015-10-08 11:11:21 -0700

    tmp

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
+  set.length = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[length] = item;
+  this.length++;
 };
 
 setPrototype.contains = function(item) {
+  for(var i in this._storage){
+    if(this._storage[i] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for(var i in set._storage){
+    if(set._storage[i] === item){
+      delete set._storage[i];
+      set.length--;
+    }
+  }
 };
 
 /*
